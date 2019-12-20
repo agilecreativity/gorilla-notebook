3e9823532d92379950d6e9b840c69a31ec2b76d7
Andreas Steffan <a.steffan@contentreich.de>
2019-12-20 16:55:22 +0100

    Use keyword vector css
    Restored font awesome / added to package.json

--- src/clj/pinkgorilla/worksheet/code_cell_menu.cljs
+++ src/clj/pinkgorilla/worksheet/code_cell_menu.cljs
@@ -4,8 +4,8 @@
 
 
 (defn cell-menu [segment]
-  [:div {:class "font-sans flex flex-col text-center sm:flex-row sm:text-left sm:justify-between  px-6 bg-white sm:items-baseline w-full"}
-   
+  [:div {:class [:font-sans :flex :flex-col :text-center :sm:flex-row :sm:text-left :sm:justify-between :px-6 :bg-white :sm:items-baseline :w-full]}
+
    ;; Kernel (clj/cljs)
    [:div.mb-1.bg-yellow-300.shadow
     [:p.text-lg.p-1  {:on-click #(dispatch [:app:kernel-toggle])}
@@ -13,15 +13,15 @@
 
    [:div.bg-green-700.mt-1.mb-1.h-8
    ;[:p {:class "text-lg no-underline text-grey-darkest hover:text-blue-dark ml-2"} "One" ]
-    
-    [:a {:class "block lg:inline-block mt-0 text-teal-200 hover:bg-orange-500 mr-4 p-1"
+
+    [:a {:class [:pg-cell-actions :lg:inline :hover:bg-orange-500]
          :on-click #(do
                       (dispatch [:worksheet:evaluate])
                       ;(rf/dispatch [::events/set-navbar-menu-active? false])
                       )}
      "evaluate"]
 
-    [:a {:class "block lg:inline-block mt-0 text-teal-200 hover:bg-orange-500 mr-4 p-1"
+    [:a {:class [:pg-cell-actions :lg:inline :hover:bg-orange-500]
          :on-click #(do
                       (dispatch [:worksheet:clear-output])
                       ;(rf/dispatch [::events/set-navbar-menu-active? false])
@@ -30,11 +30,11 @@
 
    ;; Move Segment around.
    [:div.text-lg
-    [:a {:class "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:orange-500 mr-4 blue-700"
+    [:a {:class    [:pg-cell-move :lg:inline-block :hover:orange-500 :lg:mt-0]
          :on-click #(dispatch [:worksheet:moveUp])}
      [:i.fas.fa-arrow-circle-up]
      ]
-    [:a {:class "block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:orange-500 mr-4"
+    [:a {:class    [:pg-cell-move :lg:inline-block :hover:orange-500 :lg:mt-0]
          :on-click #(dispatch [:worksheet:moveDown])}
        [:i.fas.fa-arrow-circle-down]
      ]]
